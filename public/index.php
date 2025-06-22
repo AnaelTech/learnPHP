58ec5035d977a4d446ff131e4c4cac27958186b0
Anael Payet <anaelpayetpro@gmail.com>
2025-06-22 11:23:57 +0200

    ✨ feat : Add composer and autoload

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Main entry point for the application.
+ *
+ * This file is loaded by the web server and is responsible for setting up the
+ * application environment and routing requests to the appropriate controllers.
+ *
+ * PHP version 8.3.6
+ *
+ * @category Application
+ * @package  App
+ * @author   AnaelTech <anael.tech@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @date     2023-06-22
+ * @version  GIT: main
+ * @link     https://github.com/anaeltech/learnPHP
+ */
